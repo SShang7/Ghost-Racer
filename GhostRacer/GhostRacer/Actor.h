21f86859656169431a40b6547b244bc321bd0ed1
Sammy Shang <sammy7shang@gmail.com>
2021-02-27 21:28:12 -0800

    comments

--- GhostRacer/GhostRacer/Actor.h
+++ GhostRacer/GhostRacer/Actor.h
@@ -22,34 +22,34 @@ protected:
 	StudentWorld* getWorld() const;
 public:
 	Actor(StudentWorld* world, int imageID, double startX, double startY, double size, unsigned int depth, int startDirection, bool coll, int hp, int speedv, int speedh);
-	virtual void doSomething() = 0;
-	virtual bool sprayed();
-	int getHP() const;
-	void loseHP(int n);
-	void setHP(int n);
-	bool getLiving() const;
-	void kill();
-	double getSpeedV() const;
-	bool getColl() const;
+	virtual void doSomething() = 0; //pure virtual function because actors deriving from this class will have their own doSomethings
+	virtual bool sprayed(); //defaults to return false for objects that don't interact with the Holy Water projectile. Virtual because certain actors have their own sprayed interactions.
+	int getHP() const; //gets the HP of the actor
+	void loseHP(int n); //subtracts n from HP for when an actor takes damage
+	void setHP(int n); //sets the HP to the value of n
+	bool getLiving() const; //lets you know if the Actor is still alive
+	void kill();//Turns the Actor's m_living = false;
+	double getSpeedV() const;// gets the vertical speed of the actor
+	bool getColl() const;// gets if the actor is a collision-avoidance worthy actor
 };
 
 
 //Car Class
 class Car : public Actor {
 	public:
-		Car(StudentWorld* world, int imageID, double startX, double startY, int hp, int speedv);
-		virtual void doSomething() = 0;
+		Car(StudentWorld* world, int imageID, double startX, double startY, int hp, int speedv); //Constructor for Car. All cars have the same size = 4.0, direction = 90, and are collision-avoidance worthy actors.
+		virtual void doSomething() = 0; //Pure virtual because each Car has it's own doSomething.
 };
 
 
 //GhostRacer Class
 class GhostRacer : public Car {
 public:
-	GhostRacer(StudentWorld* world);
-	virtual void doSomething();
-	void addWater();
-	void spin();
-	int getWater() const;
+	GhostRacer(StudentWorld* world); //constructor for GhostRacer. GhostRacer already has imageID, an x and y, hp , and speedv.
+	virtual void doSomething(); //GhostRacer's doSomething. Checks for keypresses and moves the GhostRacer.
+	void addWater(); //addWater increments the number of sprays by 10.
+	void spin(); //movement interaction for GhostRacer when it touches an OilSlick
+	int getWater() const; //gets the number of sprays that GhostRacer has left.
 private:
 	int m_water;
 };
@@ -58,8 +58,8 @@ private:
 class Spray : public Actor
 {
 public:
-	Spray(StudentWorld* world, double x, double y, int dir);
-	virtual void doSomething();
+	Spray(StudentWorld* world, double x, double y, int dir); //constructor for Spray. Spray gets the world, x, y, and dir from GhostRacer doSomething.
+	virtual void doSomething(); //updates the spray movement and checks if the spray hits any other actors.
 private:
 	int m_travelDist;
 };
@@ -67,56 +67,56 @@ private:
 //Stationary Class
 class Stationary : public Actor {
 public:
-	Stationary(StudentWorld* world, int imageID, double startX, double startY, double size, int dir);
+	Stationary(StudentWorld* world, int imageID, double startX, double startY, double size, int dir); //Stationary Constructor for elements that don't move.
 
 };
 
 //Border Class
 class Border : public Stationary {
 public:
-	Border(StudentWorld* world, bool yellow, int x, int y);
-	virtual void doSomething();
+	Border(StudentWorld* world, bool yellow, int x, int y); //Border constructor that takes in the world, if the border should be yellow or white, and x and y positions.
+	virtual void doSomething(); //Border's doSomething which is to just stay still.
 private:
 	int isYellowImage(bool yellow);
 };
 //class OilSlick
 class OilSlick : public Stationary {
 public: 
-	OilSlick(StudentWorld* world, double x, double y);
-	virtual void doSomething();
+	OilSlick(StudentWorld* world, double x, double y); //OilSlick constructor that takes in world, x, and y. Makes size randInt(2,5).
+	virtual void doSomething(); //OilSlick's doSomething stays still and checks for collisions with GhostRacer. Calls GhostRacer's spin if collide.
 };
 //HealingGoodie Class
 class HealingGoodie : public Stationary
 {
 public:
-	HealingGoodie(StudentWorld* world, double x, double y);
-	virtual void doSomething();
-	virtual bool sprayed();
+	HealingGoodie(StudentWorld* world, double x, double y); //HealingGoodie constructor that takes in world, x, and y from dead ZombiePed's world, x, and y.
+	virtual void doSomething(); //HealingGoodie doSomething stays still and checks for collisions with GhostRacer. Heals GhostRacer if collide. Makes sure no overheal > 100hp and increase score.
+	virtual bool sprayed(); //calls kill() and returns true if sprayed.
 };
 //HolyWaterGoodie Class
 class HolyWaterGoodie : public Stationary
 {
 public:
-	HolyWaterGoodie(StudentWorld* world, double x, double y);
-	virtual void doSomething();
-	virtual bool sprayed();
+	HolyWaterGoodie(StudentWorld* world, double x, double y); //HolyWaterGoodie constructor that takes in world, x, and y from StudentWorld's move().
+	virtual void doSomething();//HolyWaterGoodie doSomething stays still and checks for collisions with GhostRacer. Adds water to GhostRacer if collide and increase score.
+	virtual bool sprayed();//calls kill() and returns true if sprayed.
 };
 
 //Soul Class
 class Soul : public Stationary
 {
 public:
-	Soul(StudentWorld* world, double x, double y);
-	virtual void doSomething();
+	Soul(StudentWorld* world, double x, double y);//Soul constructor that takes in world, x, and y from StudentWorld's move().
+	virtual void doSomething();//Soul doSomething stays still and rotates direction. Checks for collisions with GhostRacer. Adds a soul to StudentWorld if collide and increase score.
 };
 
 
 //ZombieCab Class
 class ZombieCab : public Car {
 public:
-	ZombieCab(StudentWorld* world, double x, double y, int speedv);
-	virtual void doSomething();
-	virtual bool sprayed();
+	ZombieCab(StudentWorld* world, double x, double y, int speedv); //ZombieCab constructor that takes in world, x, y, and speedv from StudentWorld move().
+	virtual void doSomething(); //doSomething updates its position and has ZombieCab's movement algorithms.
+	virtual bool sprayed();//loses one hp if hit by water. if killed by water has chance to drop oil spill.
 private:
 	bool m_damaged;
 	int m_plan;
@@ -125,14 +125,13 @@ private:
 //Pedestrian Class
 class Pedestrian : public Actor {
 public:
-	Pedestrian(StudentWorld* world, int imageID, double startX, double startY, double size);
-	virtual void doSomething() = 0;
-	virtual bool sprayed() = 0;
-	int getMove() const;
-	void setMove(int n);
+	Pedestrian(StudentWorld* world, int imageID, double startX, double startY, double size); //Pedestrian constructor that initializes common values for Pedestrians and starts move_plan at 0
+	virtual void doSomething() = 0; // doSomething is pure virtual because each Pedestrian has it's own doSomething
 protected:
 	void moveDec();
 	void newPlan();
+	int getMove() const;
+	void setMove(int n);
 private:
 	int m_plan;
 };
@@ -140,17 +139,17 @@ private:
 //HumanPed Class
 class HumanPed : public Pedestrian {
 public:
-	HumanPed(StudentWorld* world, double x, double y);
-	virtual void doSomething();
-	virtual bool sprayed();
+	HumanPed(StudentWorld* world, double x, double y); //constructor for HumanPed at a certain x and y from StudentWorld
+	virtual void doSomething(); //doSomething for HumanPed that updates its movement and detects if it is hit by a GhostRacer
+	virtual bool sprayed(); //sprayed makes it reverse direction and returns true
 };
 
 //ZombiePed Class
 class ZombiePed : public Pedestrian {
 public:
-	ZombiePed(StudentWorld* world, double x, double y);
-	virtual void doSomething();
-	virtual bool sprayed();
+	ZombiePed(StudentWorld* world, double x, double y); //constructor for ZombiePed at a certain x and y from StudentWorld
+	virtual void doSomething();//doSomething for ZombiePed that updates its movement and detects if it is hit by a GhostRacer
+	virtual bool sprayed(); //sprayed makes it take 1hp damage and if it kills there is a chance of heal goodie drop. Returns true.
 private:
 	int m_grunt;
 };
--- GhostRacer/GhostRacer/StudentWorld.h
+++ GhostRacer/GhostRacer/StudentWorld.h
@@ -18,15 +18,13 @@ public:
     virtual int init(); //initializes world
     virtual int move(); //updates each tick
     virtual void cleanUp(); //removes all the allocated memory
-    GhostRacer* getGR() const; //gets point to Ghost Racer
-    void addActor(Actor* a);
-    void addSoul();
-    bool overlapWater(Actor* a);
-    Actor* closestFrontCollLane(Actor* a);
-    Actor* closestBehindCollLane(Actor* a);
-    // If actor a overlaps this world's GhostRacer, return a pointer to the
-    // GhostRacer; otherwise, return nullptr
-    GhostRacer* getOverlappingGhostRacer(Actor* a) const;
+    GhostRacer* getGR() const; //gets point to Ghost Racer. Allows other classes to modify and access GhostRacer.
+    void addActor(Actor* a); //adds an Actor the the end of the list of actors. Allows for creation of Spray, HealingGoodie, and OilSlick.
+    void addSoul(); //increments m_soul, the soul count. Specifically used for collection of SoulGoodie.
+    bool overlapWater(Actor* a); //checks if an actor is overlapping with spray projectile. Useful for dealing with collisions between Spray and other Actors.
+    Actor* closestFrontCollLane(Actor* a); //finds the closest collision-avoidance worth actor in front of a ZombieCab within 96 pixels. Returns nullptr if none found. Useful for ZombieCab movement in doSomething.
+    Actor* closestBehindCollLane(Actor* a); //finds the closest collision-avoidance worth actor behind a ZombieCab within 96 pixels. Returns nullptr if none found. Useful for ZombieCab movement in doSomething.
+    GhostRacer* getOverlappingGhostRacer(Actor* a) const; // If actor a overlaps this world's GhostRacer, return a pointer to the GhostRacer; otherwise, return nullptr. Useful for dealing with collisions between GhostRacer and other Actors.
 private:
     std::list<Actor*> aloa;
     GhostRacer* m_gr;
